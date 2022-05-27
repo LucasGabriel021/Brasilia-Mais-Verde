94f30fdd9c9e64c0316ec83b8e91ab4ca9550f94
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-05-26 22:46:26 -0300

    patch 1.5

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,16 @@ let rellax = new Rellax('.parallax');
 
 // Botão toggle Hamburguer
 const HAMBURGUER_MENU = document.querySelector('.btn-hamburguer');
+let listMenu = document.querySelector('.menu');
 
 const MENU_ACTIVE = () => {
      HAMBURGUER_MENU.classList.toggle('active');
+     if(listMenu.classList.contains('show') == true) {
+          listMenu.classList.remove('show');
+     }
+     else {
+          listMenu.classList.add('show');
+     }
 }
 
 HAMBURGUER_MENU.addEventListener('click', MENU_ACTIVE);
