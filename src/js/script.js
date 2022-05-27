d0b887322307867cea383e8731b0293e7125f848
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-05-26 21:21:06 -0300

    patch 1.5

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 // Efeito das palavras Parallax
 let rellax = new Rellax('.parallax');
 
+// Botão toggle Hamburguer
+const HAMBURGUER_MENU = document.querySelector('.btn-hamburguer');
+
+const MENU_ACTIVE = () => {
+     HAMBURGUER_MENU.classList.toggle('active');
+}
+
+HAMBURGUER_MENU.addEventListener('click', MENU_ACTIVE);
