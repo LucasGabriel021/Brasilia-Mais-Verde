40318b5de39d050b358e7da4d2e9305cca806e93
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-06-20 22:19:06 -0300

    patch 2.5

--- src/js/login.js
+++ src/js/login.js
@@ -4,9 +4,11 @@ let formCadastro = document.getElementById("form-modal--cadastro");
 function alternarCadastro() {
      formLogin.style.display = "none";
      formCadastro.style.display = "flex";
+     document.title = "Cadastro";
 }
 
 function alternarLogin() {
      formCadastro.style.display = "none";
      formLogin.style.display = "flex";
+     document.title = "Login";
 }
